e901de8a2be5bbea089ddb77c47687b12213d901
arightruckuss <66334071+arightruckuss@users.noreply.github.com>
2020-10-25 22:38:36 +0000

    Add files via upload

--- Gazetteer/js/gazetteerjs.js
+++ Gazetteer/js/gazetteerjs.js
@@ -1,18 +1,44 @@
 //initialize map
-var map = L.map('mapid', {
-	center:[40, 1], 
-	zoom: 1,
+var map = L.map('map', {
+	center:[45, 20], 
+    zoom: 2,
 })
 
-function satelliteMap(){
+
 L.tileLayer('https://api.maptiler.com/maps/hybrid/{z}/{x}/{y}.jpg?key=wJnNNBBw35SQm7Zc7Ptd', {
     maxZoom: 18,
     minZoom: 2,
     tileSize: 512,
     zoomOffset: -1,
     }).addTo(map);
-}
-satelliteMap();
+
+L.geoJSON(borders, {
+    style: function(feature) {
+    return {
+        fillOpacity: 0,
+        stroke: true,
+        color: "blue", // Lines in between countries.
+        weight: 1
+        };
+    }
+    }).bindPopup(function(layer) {
+    return layer.feature.properties.name;
+    }).addTo(map);
+
+//initialize map
+var map = L.map('map', {
+	center:[45, -122], 
+    zoom: 3,
+})
+
+function satelliteMap(){
+    L.tileLayer('https://api.maptiler.com/maps/hybrid/{z}/{x}/{y}.jpg?key=wJnNNBBw35SQm7Zc7Ptd', {
+        maxZoom: 18,
+        minZoom: 2,
+        tileSize: 512,
+        zoomOffset: -1,
+        }).addTo(map);
+    }
 
 function streetMap(){
     L.tileLayer('https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=wJnNNBBw35SQm7Zc7Ptd', {
@@ -41,11 +67,76 @@ function blackWhiteMap(){
         }).addTo(map);
 }
 
+//Meter maps
+function hillShadeMap(){
+    L.tileLayer('https://api.maptiler.com/tiles/hillshades/{z}/{x}/{y}.png?key=wJnNNBBw35SQm7Zc7Ptd', {
+        maxZoom: 18,
+        minZoom: 2,
+        tileSize: 512,
+        zoomOffset: -1,
+        }).addTo(map);
+
+        var value = parseInt(document.getElementById('hillMeter').value, 12);
+        value = isNaN(value) ? value : value;
+        value++;
+        document.getElementById('hillMeter').value = value;
+}
+
+function zeroMeter(){
+    document.getElementById('hillMeter').value = 0;
+    document.getElementById('precipitationMeter').value = 0;
+    document.getElementById('cloudMeter').value = 0;
+    document.getElementById('tempMeter').value = 0; 
+}
+
+//Weather maps
+function precipitationMap(){
+    L.tileLayer('https://tile.openweathermap.org/map/precipitation_new/{z}/{x}/{y}.png?appid=513dd1747f3b4fd4eeb27d17169c8593', {
+        maxZoom: 18,
+        minZoom: 2,
+        tileSize: 512,
+        zoomOffset: -1,
+        }).addTo(map);
+    
+    var value = parseInt(document.getElementById('precipitationMeter').value, 8);
+    value = isNaN(value) ? value : value;
+    value++;
+    document.getElementById('precipitationMeter').value = value;
+}
+
+function cloudMap(){
+    L.tileLayer('https://tile.openweathermap.org/map/clouds_new/{z}/{x}/{y}.png?appid=513dd1747f3b4fd4eeb27d17169c8593', {
+        maxZoom: 18,
+        minZoom: 2,
+        tileSize: 512,
+        zoomOffset: -1,
+        }).addTo(map);
+        
+        var value = parseInt(document.getElementById('cloudMeter').value, 12);
+        value = isNaN(value) ? value : value;
+        value++;
+        document.getElementById('cloudMeter').value = value; 
+    }
+
+function tempMap(){
+    L.tileLayer('https://tile.openweathermap.org/map/temp_new/{z}/{x}/{y}.png?appid=513dd1747f3b4fd4eeb27d17169c8593', {
+        maxZoom: 18,
+        minZoom: 2,
+        tileSize: 512,
+        zoomOffset: -1,
+        }).addTo(map);
+
+        var value = parseInt(document.getElementById('tempMeter').value, 8);
+        value = isNaN(value) ? value : value;
+        value++;
+        document.getElementById('tempMeter').value = value; 
+    }
+
 function showMapChoice() {
      document.getElementById("mapChoice").style.visibility = "visible";
 }
 
-/*Country list*/
+//Country list
 function countryList(){
     $.get("php/gazetteerphp.php",
     function(data){
@@ -53,7 +144,7 @@ function countryList(){
     });
 }
 
-/*Menu box display*/
+//Menu box display
 function menuButton() {
     var x = document.getElementById("mapChoice");
     if (x.style.display == "none") {
@@ -62,3 +153,6 @@ function menuButton() {
       x.style.display = "none";
     }
   }
+
+
+    
--- Gazetteer/php/gazetteerphp.php
+++ Gazetteer/php/gazetteerphp.php
@@ -1,12 +1,58 @@
-<?php
 
-    $url = json_decode(file_get_contents('countryBorders.geo.json'));
+<?php 
 
-    $feature = $url->features;
+ 
 
-    foreach ($feature as $features){
+// next two lines for testing only
 
-        $cities = $features->properties->name;
-        print_r($cities);
-    }
+
+
+ini_set('display_errors', 'On');
+
+error_reporting(E_ALL);
+
+
+
+$countries = json_decode(file_get_contents('countryBorders.geo.json'),true);
+
+
+
+$countriesParsed = [];
+
+
+foreach ($countries['features'] AS $country) {
+
+    
+
+    $temp = [];
+
+    $temp['code']=$country['properties']['iso_a3'];
+
+    $temp['name']=$country['properties']['name'];
+
+    array_push($countriesParsed, $temp);
+
+    $object = json_decode(json_encode($temp), false);
+
+    echo '<option>' . $object->name . '</option>';
+
+}
+
+
+usort($countriesParsed, function ($item1, $item2) {
+
+    return $item1['name'] <=> $item2['name'];
+
+});
+
+$output['status']['code'] = "200";
+
+$output['status']['name'] = "ok";
+
+$output['status']['description'] = "success";
+
+$output['data'] = $countriesParsed;
+
+header('Content-Type: application/json; charset=UTF-8');
+
 ?>
